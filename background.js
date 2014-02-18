1c0f48945d21a31918fede2a9fd75c653ad3bab3
Scott Glajch <glajchs@gmail.com>
2014-02-18 00:51:39 -0500

    Version 1.0.  Adds IFrame support and fixes bugs where the video would restart playback upon maximizing.

--- background.js
+++ background.js
@@ -1,12 +1,112 @@
 chrome.pageAction.onClicked.addListener(function(tab) {
-    chrome.tabs.sendMessage(tab.id, { "requestType": "pageActionClicked" }, null);
+    var existingTabIndex = _.findIndex(biggestFlashPerTab, function(tabObject) {
+        return (tabObject.tabId === tab.id);
+    });
+    var actionType = "maximize";
+    if (typeof biggestFlashPerTab[existingTabIndex] === "object" && biggestFlashPerTab[existingTabIndex].isAlreadyMaximized === true) {
+        actionType = "restore";
+    }
+    if (existingTabIndex !== -1) {
+        biggestFlashPerTab.splice(existingTabIndex, 1);
+    }
+
+    chrome.tabs.sendMessage(tab.id, { "requestType": "pageActionClicked", "actionType": actionType }, function(response) {});
 });
+
+var biggestFlashPerTab = [];
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         var requestType = request.requestType;
         if (requestType == "initializePageActionIcon") {
+            var existingTabIndex = _.findIndex(biggestFlashPerTab, function(tabObject) {
+                return (tabObject.tabId === sender.tab.id);
+            });
+            if (existingTabIndex !== -1) {
+                biggestFlashPerTab.splice(existingTabIndex, 1);
+            }
             chrome.pageAction.show(sender.tab.id);
+        } else if (requestType == "resizeFlash") {
+            var existingTabIndex = _.findIndex(biggestFlashPerTab, function(tabObject) {
+                return (tabObject.tabId === sender.tab.id);
+            });
+            if (existingTabIndex == -1) {
+                return;
+            }
+            biggestFlashPerTab[existingTabIndex].windowWidth = request.windowWidth;
+            biggestFlashPerTab[existingTabIndex].windowHeight = request.windowHeight;
+            sendResizeEventToTab(sender.tab.id);
+        } else if (requestType == "areaOfFlashInFrame") {
+            var existingTabIndex = _.findIndex(biggestFlashPerTab, function(tabObject) {
+                return (tabObject.tabId === sender.tab.id);
+            });
+            if (existingTabIndex == -1) {
+                var timeoutId = -1;
+                if (request.isFrameTop === true) {
+                    timeoutId = setTimeout(function() {
+                        sendResponseToTab(sender.tab.id);
+                    }, 20);
+                }
+                var tabData = { tabId: sender.tab.id, href: request.href, area: request.area, originalAspectRatio: request.originalAspectRatio, timeoutId: timeoutId };
+                if (request.isFrameTop === true) {
+                    tabData.windowWidth = request.windowWidth;
+                    tabData.windowHeight = request.windowHeight;
+                }
+                biggestFlashPerTab.push(tabData);
+            } else {
+                if (biggestFlashPerTab[existingTabIndex].area < request.area) {
+                    biggestFlashPerTab[existingTabIndex].area = request.area;
+                    biggestFlashPerTab[existingTabIndex].href = request.href;
+                }
+                if (request.isFrameTop === true) {
+                    biggestFlashPerTab[existingTabIndex].windowWidth = request.windowWidth;
+                    biggestFlashPerTab[existingTabIndex].windowHeight = request.windowHeight;
+                }
+                if (biggestFlashPerTab[existingTabIndex].area > 1 && biggestFlashPerTab[existingTabIndex].windowWidth != null) {
+                    clearTimeout(biggestFlashPerTab[existingTabIndex].timeoutId);
+                    var timeoutId = setTimeout(function() {
+                        sendResponseToTab(sender.tab.id);
+                    }, 20);
+                    biggestFlashPerTab[existingTabIndex].timeoutId = timeoutId;
+                }
+            }
         }
     }
 );
+
+function sendResponseToTab(tabId) {
+    var existingTabIndex = _.findIndex(biggestFlashPerTab, function(tabObject) {
+        return (tabObject.tabId === tabId);
+    });
+    if (existingTabIndex == -1) {
+        return;
+    }
+    chrome.tabs.sendMessage(tabId, {
+        "requestType": "maximizeFlashForThisFrame",
+        href: biggestFlashPerTab[existingTabIndex].href,
+        windowWidth: biggestFlashPerTab[existingTabIndex].windowWidth,
+        windowHeight: biggestFlashPerTab[existingTabIndex].windowHeight,
+    });
+    biggestFlashPerTab[existingTabIndex] = {
+        tabId: tabId,
+        href: biggestFlashPerTab[existingTabIndex].href,
+        originalAspectRatio: biggestFlashPerTab[existingTabIndex].originalAspectRatio,
+        isAlreadyMaximized: true
+    };
+}
+
+function sendResizeEventToTab(tabId) {
+    var existingTabIndex = _.findIndex(biggestFlashPerTab, function(tabObject) {
+        return (tabObject.tabId === tabId);
+    });
+    if (existingTabIndex == -1) {
+        return;
+    }
+    chrome.tabs.sendMessage(tabId, {
+        "requestType": "resizeFlashForFrame",
+        href: biggestFlashPerTab[existingTabIndex].href,
+        originalAspectRatio: biggestFlashPerTab[existingTabIndex].originalAspectRatio,
+        windowWidth: biggestFlashPerTab[existingTabIndex].windowWidth,
+        windowHeight: biggestFlashPerTab[existingTabIndex].windowHeight,
+    });
+}
--- maximize_flash.js
+++ maximize_flash.js
@@ -1,19 +1,162 @@
 var elementToKeepMaximized = null;
+var originalData = {
+    element: {},
+    parent: {},
+    iframe: {},
+    iframeURL: ""
+};
+var windowWidth = 0;
+var windowHeight = 0;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.requestType == "pageActionClicked") {
-        if (elementToKeepMaximized == null) {
-            initMaximizeFlash();
+        if (request.actionType == "maximize") {
+            findLargestFlashForThisFrame();
+            var area = $(elementToKeepMaximized).width() * $(elementToKeepMaximized).height();
+            if (area > 1 || window.self == window.top) {
+                chrome.runtime.sendMessage({
+                    requestType: "areaOfFlashInFrame",
+                    isFrameTop: window.self == window.top,
+                    windowWidth: $(window).width(),
+                    windowHeight: $(window).height(),
+                    href: window.location.href,
+                    area: $(elementToKeepMaximized).width() * $(elementToKeepMaximized).height(),
+                    originalAspectRatio: originalData.originalAspectRatio
+                });
+            }
         } else {
-            restoreFlash();
+            if (window.self == window.top) {
+                $(window).unbind("resize", resizeFlash);
+            }
+            if (elementToKeepMaximized != null) {
+                restoreFlash();
+            } else if (window.self == window.top) {
+                _.forEach($("iframe"), function(iframe) {
+                    if (iframe.src == originalData.iframeURL) {
+                        _.forEach(propertiesToManageForElementAndParent, function(cssProperty) {
+                            restoreCSSValue(iframe, originalData.iframe, cssProperty);
+                        });
+                    }
+                });
+            }
+            
+        }
+    } else if (request.requestType == "maximizeFlashForThisFrame") {
+        if (window.self == window.top) {
+            var iframes = $("iframe");
+            _.forEach(iframes, function(iframe) {
+                if (iframe.src == request.href) {
+                    originalData.iframeURL = request.href;
+                    var parentElement = $(elementToKeepMaximized).parent();
+                    _.forEach(propertiesToManageForElementAndParent, function(cssProperty) {
+                        originalData.iframe[cssProperty] = $(iframe).get(0).style[cssProperty];
+                    });
+                    var stylesToSetOnIFrame = {
+                        height: request.windowHeight + "px",
+                        width: request.windowWidth + "px",
+                        border: "none",
+                        margin: "0px",
+                        position: "fixed",
+                        "z-index": "2147483647",
+                        top: "0px",
+                        left: "0px",
+                    };
+                    setStylesWithImportantFlag(iframe, stylesToSetOnIFrame);
+                }
+            });
+            $(window).bind("resize", resizeFlash);
+        }
+        if (window.location.href == request.href) {
+            windowWidth = request.windowWidth;
+            windowHeight = request.windowHeight;
+            initMaximizeFlash();
+        }
+    } else if (request.requestType == "resizeFlashForFrame") {
+        if (window.self == window.top) {
+            var iframes = $("iframe");
+            _.forEach(iframes, function(iframe) {
+                if (iframe.src == request.href) {
+                    windowWidth = request.windowWidth;
+                    windowHeight = request.windowHeight;
+                    var windowAspectRatio = windowWidth / windowHeight;
+                    var newWidth = 0;
+                    var newHeight = 0;
+                    if (request.originalAspectRatio <= windowAspectRatio) {
+                        newHeight = windowHeight;
+                        newWidth = Math.floor(windowHeight * request.originalAspectRatio);
+                    } else {
+                        newHeight = Math.floor(windowWidth / request.originalAspectRatio);
+                        newWidth = windowWidth;
+                    }
+                    setStyleWithImportantFlag(iframe, "width", newWidth + "px");
+                    setStyleWithImportantFlag(iframe, "height", newHeight + "px");
+                }
+            });
+        }
+        if (window.location.href == request.href) {
+            windowWidth = request.windowWidth;
+            windowHeight = request.windowHeight;
+            var windowAspectRatio = windowWidth / windowHeight;
+            var newWidth = 0;
+            var newHeight = 0;
+            if (originalData.originalAspectRatio <= windowAspectRatio) {
+                newHeight = windowHeight;
+                newWidth = Math.floor(windowHeight * originalData.originalAspectRatio);
+            } else {
+                newHeight = Math.floor(windowWidth / originalData.originalAspectRatio);
+                newWidth = windowWidth;
+            }
+            setStyleWithImportantFlag(elementToKeepMaximized, "width", newWidth + "px");
+            setStyleWithImportantFlag(elementToKeepMaximized, "height", newHeight + "px");
+            if ($(elementToKeepMaximized).css("position") === "relative") {
+                var parentElement = $(elementToKeepMaximized).parent();
+                setStyleWithImportantFlag(parentElement, "width", newWidth + "px");
+                setStyleWithImportantFlag(parentElement, "height", newHeight + "px");
+            }
+            // maximizeFlash();
         }
     }
 });
 
-chrome.runtime.sendMessage({ requestType: "initializePageActionIcon" }, function() {});
+if (window.self == window.top) {
+    chrome.runtime.sendMessage({ requestType: "initializePageActionIcon" }, function() {});
+}
 
+function resizeFlash() {
+    if (elementToKeepMaximized != null) {
+        // resize w/out background page request
+        windowWidth = $(window).width();
+        windowHeight = $(window).height();
+        var windowAspectRatio = windowWidth / windowHeight;
+        var newWidth = 0;
+        var newHeight = 0;
+        if (originalData.originalAspectRatio <= windowAspectRatio) {
+            newHeight = windowHeight;
+            newWidth = Math.floor(windowHeight * originalData.originalAspectRatio);
+        } else {
+            newHeight = Math.floor(windowWidth / originalData.originalAspectRatio);
+            newWidth = windowWidth;
+        }
+        setStyleWithImportantFlag(elementToKeepMaximized, "width", newWidth + "px");
+        setStyleWithImportantFlag(elementToKeepMaximized, "height", newHeight + "px");
+        if ($(elementToKeepMaximized).css("position") === "relative") {
+            var parentElement = $(elementToKeepMaximized).parent();
+            setStyleWithImportantFlag(parentElement, "width", newWidth + "px");
+            setStyleWithImportantFlag(parentElement, "height", newHeight + "px");
+        }
+    } else {
+        // resize w/background page request
+        chrome.runtime.sendMessage({
+            requestType: "resizeFlash",
+            windowWidth: $(window).width(),
+            windowHeight: $(window).height()
+        }, function() {});
+    }
+}
 
-function initMaximizeFlash() {
+var propertiesToManageForElementAndParent = [ "width", "height", "position", "z-index", "top", "left", "margin", "border" ];
+
+function findLargestFlashForThisFrame() {
     var possibleElements = $("object, embed");
     if (possibleElements.size() == 0) {
         return;
@@ -21,7 +164,7 @@ function initMaximizeFlash() {
         elementToKeepMaximized = possibleElements.get(0);
     } else {
         var currentWinnerElement = possibleElements.get(0);
-        for (var i = 1 ; i < possibleElements.size(); i++) {
+        for (var i = 1; i < possibleElements.size(); i++) {
             var currentWinnerElementArea = $(currentWinnerElement).width() * $(currentWinnerElement).height();
             var possibleWinnerElementArea = $(possibleElements.get(i)).width() * $(possibleElements.get(i)).height();
             if (possibleWinnerElementArea > currentWinnerElementArea) {
@@ -31,67 +174,107 @@ function initMaximizeFlash() {
         elementToKeepMaximized = currentWinnerElement;
     }
 
-    $(elementToKeepMaximized).get(0).style.originalWidth = $(elementToKeepMaximized).getElementStyle("width");
-    $(elementToKeepMaximized).get(0).style.originalHeight = $(elementToKeepMaximized).getElementStyle("height");
-    $(elementToKeepMaximized).get(0).style.originalPosition = $(elementToKeepMaximized).getElementStyle("position");
-    $(elementToKeepMaximized).get(0).style.originalZIndex = $(elementToKeepMaximized).getElementStyle("z-index");
-    $(elementToKeepMaximized).get(0).style.originalTop = $(elementToKeepMaximized).getElementStyle("top");
-    $(elementToKeepMaximized).get(0).style.originalLeft = $(elementToKeepMaximized).getElementStyle("left");
+    var originalWidth = $(elementToKeepMaximized).width();
+    var originalHeight = $(elementToKeepMaximized).height();
+    originalData.originalAspectRatio = originalWidth / originalHeight;
+}
+
+function initMaximizeFlash() {
     maximizeFlash();
-    $(window).bind("resize", maximizeFlash);
 }
 
-function endsWith(stringToCompare, substring) {
-    var indexOfSubstring = stringToCompare.lastIndexOf(substring);
-    return (indexOfSubstring != -1 && (indexOfSubstring + substring.length == stringToCompare.length));
-}
-
-function restoreCSSValue(originalCssValue, cssProperty) {
-    if (originalCssValue == null) {
-        $(elementToKeepMaximized).important(cssProperty, false);
-        $(elementToKeepMaximized).css(cssProperty, "");
-    } else if (endsWith(originalCssValue, "!important;")) {
-        var newStr = originalCssValue.substring(0, originalCssValue.lastIndexOf("!important;") - 1);
+function restoreCSSValue(element, original, cssProperty) {
+    var originalStyle = original[cssProperty];
+    if (originalStyle == null) {
+        $(element).important(cssProperty, false);
+        $(element).css(cssProperty, "");
     } else {
-        var newStr = originalCssValue.substring(0, originalCssValue.lastIndexOf(";"));
+        $(element).important(cssProperty, false);
+        $(element).css(cssProperty, originalStyle);
     }
 }
 
 function restoreFlash() {
-    $(window).unbind("resize", maximizeFlash);
-    restoreCSSValue($(elementToKeepMaximized).get(0).style.originalWidth, "width");
-    restoreCSSValue($(elementToKeepMaximized).get(0).style.originalHeight, "height");
-    restoreCSSValue($(elementToKeepMaximized).get(0).style.originalPosition, "position");
-    restoreCSSValue($(elementToKeepMaximized).get(0).style.originalZIndex, "z-index");
-    restoreCSSValue($(elementToKeepMaximized).get(0).style.originalTop, "top");
-    restoreCSSValue($(elementToKeepMaximized).get(0).style.originalLeft, "left");
-    elementToKeepMaximized = null;
+    if (elementToKeepMaximized != null) {
+        var currentPosition = $(elementToKeepMaximized).css("position");
+        _.forEach(propertiesToManageForElementAndParent, function(cssProperty) {
+            restoreCSSValue(elementToKeepMaximized, originalData.element, cssProperty);
+        });
+        if (currentPosition === "relative") {
+            var parentElement = $(elementToKeepMaximized).parent();
+            _.forEach(propertiesToManageForElementAndParent, function(cssProperty) {
+                restoreCSSValue(parentElement, originalData.parent, cssProperty);
+            });
+        }
+        elementToKeepMaximized = null;
+    }
+}
+
+function getOriginalStyleAttributeName(styleAttributeName) {
+    if (styleAttributeName === "z-index") {
+        return "maximizeFlashOriginalZIndex";
+    }
+    return "maximizeFlashOriginal" + styleAttributeName[0].toUpperCase() + styleAttributeName.substring(1, styleAttributeName.length);
+}
+
+function storeOffOriginalValuesIfMissing(element, styleAttributeName) {
+    if ($(element).length == 0) {
+        return;
+    }
+    if ($.data($(element).get(0), getOriginalStyleAttributeName(styleAttributeName)) == null) {
+        $.data($(element).get(0), getOriginalStyleAttributeName(styleAttributeName), $(element).get(0).style[styleAttributeName]);
+    }
+}
+
+function setStyleWithImportantFlag(element, cssProperty, cssPropertyValue) {
+    $(element).important(cssProperty, false);
+    $(element).css(cssProperty, cssPropertyValue, true);
+}
+
+function setStylesWithImportantFlag(element, cssProperties) {
+    _.forOwn(cssProperties, function(propertyValue, propertyName) {
+        setStyleWithImportantFlag(element, propertyName, propertyValue);
+    });
 }
 
 function maximizeFlash() {
-    var originalWidth = $(elementToKeepMaximized).width();
-    var originalHeight = $(elementToKeepMaximized).height();
-    var aspectRatio = originalWidth / originalHeight;
-    var windowWidth = $(window).width();
-    var windowHeight = $(window).height();
+    var newPosition = ($(elementToKeepMaximized).css("position") === "relative" || $(elementToKeepMaximized).css("position") === "static")
+            ? "relative" : "fixed";
+    if ($(elementToKeepMaximized).css("position") === "relative" || $(elementToKeepMaximized).css("position") === "static") {
+        $(elementToKeepMaximized).css("position", "relative", true);
+    }
+
     var windowAspectRatio = windowWidth / windowHeight;
-    if (aspectRatio <= windowAspectRatio) {
-        $(elementToKeepMaximized).css({
-            "height": windowHeight + "px",
-            "width": Math.floor(windowHeight * aspectRatio) + "px",
-            "position": "fixed",
-            "z-index": "2147483647",
-            "top": "0px",
-            "left": "0px"
-        }, true);
+    var newWidth = 0;
+    var newHeight = 0;
+    if (originalData.originalAspectRatio <= windowAspectRatio) {
+        newHeight = windowHeight;
+        newWidth = Math.floor(windowHeight * originalData.originalAspectRatio);
     } else {
-        $(elementToKeepMaximized).css({
-            "height": Math.floor(windowWidth / aspectRatio) + "px",
-            "width": windowWidth + "px",
-            "position": "fixed",
-            "z-index": "2147483647",
-            "top": "0px",
-            "left": "0px"
-        }, true);
+        newHeight = Math.floor(windowWidth / originalData.originalAspectRatio);
+        newWidth = windowWidth;
+    }
+
+    var stylesToSetOnElementAndParent = {
+        height: newHeight + "px",
+        width: newWidth + "px",
+        border: "none",
+        margin: "0px",
+        position: newPosition,
+        "z-index": "2147483647",
+        top: "0px",
+        left: "0px",
+    };
+
+    _.forEach(propertiesToManageForElementAndParent, function(cssProperty) {
+        originalData.element[cssProperty] = $(elementToKeepMaximized).get(0).style[cssProperty];
+    });
+    setStylesWithImportantFlag(elementToKeepMaximized, _.merge(_.clone(stylesToSetOnElementAndParent), { position: newPosition }));
+    if (newPosition === "relative") {
+        var parentElement = $(elementToKeepMaximized).parent();
+        _.forEach(propertiesToManageForElementAndParent, function(cssProperty) {
+            originalData.parent[cssProperty] = $(parentElement).get(0).style[cssProperty];
+        });
+        setStylesWithImportantFlag(parentElement, _.merge(_.clone(stylesToSetOnElementAndParent), { position: "fixed" }));
     }
 }
